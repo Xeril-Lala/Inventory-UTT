0f59341c6f7b0aaa5944b122263b25427ee4ee3d
Tonatiuh_011 <ltonatiuh.011@gmail.com>
2023-05-12 14:35:47 -0700

    Set Procedures. DB Structure

--- env/9-TESTS.sql
+++ env/9-TESTS.sql
@@ -1,4 +1,33 @@
+USE INVENTORY_UTT;
+
 SET @MSG = NULL;
+
+CALL SET_USER(
+	'DBA',
+	'Tonatiuh',
+    'Lopez',
+    'admin',
+	'DBA',
+    TRUE,
+    @MSG
+);
+SELECT @MSG;
+
+CALL SET_USER_CONTACT(
+	'DBA',
+    '0319125293',
+    NULL,
+    'ltonatiuh.011@gmail.com',
+    'Tonatiuh.Lopez@exelaonline.com',
+    '6631226015',
+    NULL,
+    'Blvd. Bellas Artes, 19842.',
+    'DBA',
+    TRUE,
+    @MSG
+);
+SELECT @MSG;
+
 CALL SET_ASSET(
     'TEST',
     'ABC',
@@ -13,10 +42,24 @@ CALL SET_ASSET(
     TRUE,
     @MSG
 );
+CALL SET_ASSET(
+    'LOC_A',
+    'ABC_LOC',
+    'LOCATION',
+    NULL,
+    NULL,
+    'DESCRIPTION - UPDATE',
+    NULL,
+    NULL,
+    NULL,
+    'DBA',
+    TRUE,
+    @MSG
+);
 SELECT @MSG;
 
 CALL SET_INVENTORY(
-    NULL,
+    1,
     'ABCXXXX122',
     'DESCRIPTION',
     'INV_1',
@@ -42,7 +85,7 @@ SELECT @msg;
 
 SET @m_id = null;
 CALL SET_LOAN(
-    null,
+    1,
     'COMMENT - UPDATE',
     NULL,
     NOW() + 1,
@@ -54,3 +97,29 @@ CALL SET_LOAN(
     @msg
 );
 SELECT @msg result, @m_id id;
+
+SET @m_id_dtl = null;
+CALL SET_LOAN_DTL(
+    null,
+    @m_id,
+    1,
+    'Object 1',
+	'LOANED',
+    'DBA',
+    TRUE,
+    @m_id_dtl,
+	@msg
+);
+SELECT @msg, @m_id_dtl DTL_ID;
+
+CALL SET_LOAN_LOCATION(
+	@m_id,
+    'LOC_A',
+    'Ubication for test',
+    'DBA',
+    true,
+    @msg
+);
+SELECT @msg;
+
+SELECT * FROM USER_CONTACT;
