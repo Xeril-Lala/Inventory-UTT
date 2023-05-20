290de69fecc4cc6626518e708a69e4f8cc91414b
Xeril-Lala <lopezlopezabel96@gmail.com>
2023-05-19 22:13:58 -0700

    Laptop Commit 5192023

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import Sideb from './components/sidebar/sidebar.jsx';
+import Historial from './components/historial/historial.jsx';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 function App() {
   return (
     <div className="App">
-        <Sideb/>
+      
+        <Historial/>
     </div>
   );
 }
--- front-end/src/components/sidebar/sidebar.jsx
+++ front-end/src/components/sidebar/sidebar.jsx
@@ -2,24 +2,36 @@ import React, { Component } from 'react';
 import { Sidebar, Menu, MenuItem, SubMenu, menuClasses } from 'react-pro-sidebar';
 import { ProSidebarProvider } from 'react-pro-sidebar';
 import styled from 'styled-components';
-import { Zap } from '@styled-icons/boxicons-solid/Zap';
+import { Dashboard } from '@styled-icons/material-rounded/Dashboard';
+import { Calendar2PlusFill } from '@styled-icons/bootstrap/Calendar2PlusFill';
+import { DocumentBulletListClock } from '@styled-icons/fluentui-system-filled/DocumentBulletListClock';
+import { Box2Fill } from '@styled-icons/bootstrap/Box2Fill';
+import { ClipboardTaskListLtr } from '@styled-icons/fluentui-system-filled/ClipboardTaskListLtr';
+import './sidebar.css';
+
+
+
+
+
 const Sideb = () =>{
 
 
 
 
 return (
-
-    <Sidebar backgroundColor='#212121'>
-       <Menu>
-        <MenuItem prefix = "<Zap/>"> Resumen</MenuItem>
-        <MenuItem>Prestamos</MenuItem>
-        <MenuItem>Historial</MenuItem>
-        <MenuItem>Inventario</MenuItem>
-        <MenuItem>Resguardos</MenuItem>
-      </Menu>
-    </Sidebar>
-
+    <div class = "sidebarMain">
+        <Sidebar backgroundColor='#212121'>
+         <div class="logo"> UTT </div>
+         <div class="subLogo"> Inventario </div>
+        <Menu>
+            <MenuItem icon = { <Dashboard/> }> Resumen</MenuItem>
+            <MenuItem icon = { <Calendar2PlusFill/> }>Prestamos</MenuItem>
+            <MenuItem icon = { <DocumentBulletListClock/> }>Historial</MenuItem>
+            <MenuItem icon = { <Box2Fill/> }>Inventario</MenuItem>
+            <MenuItem icon = { <ClipboardTaskListLtr/> }>Resguardos</MenuItem>
+        </Menu>
+        </Sidebar>
+    </div>
 );
 };
 export default Sideb;
