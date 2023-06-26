7f1c7a9601ec41e9235c3497d2ec635cc9d4553d
Tonatiuh_011 <ltonatiuh.011@gmail.com>
2023-06-25 18:37:18 -0700

    Compose Yaml realease

--- env/set-env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Replace placeholders in appsettings.json with environment variable values
-sed -i "s/\${API_KEY}/${API_KEY}/g" ./appsettings.json
-sed -i "s/\${DB_CONNECTION}/${DB_CONNECTION}/g" ./appsettings.json
