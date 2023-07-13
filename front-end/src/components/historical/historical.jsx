c50cec5aa456fea25f2b88367142cf4705bef5e7
Xeril-Lala <lopezlopezabel96@gmail.com>
2023-07-13 15:14:54 -0700

    Desktop Commit 7132023

--- front-end/src/components/historical/historical.jsx
+++ front-end/src/components/historical/historical.jsx
@@ -1,59 +1,60 @@
 import { C } from '../../constants/C.js';
 import CustomTable from '../customTable/customTable.jsx';
-import AssetService from '../../services/Asset.js';
+import InventoryService from '../../services/Inventory.js';
 import React, { useState } from 'react';
 import '../customTable/customStyle.css';
 
 const Historical = () => {
-    const assetService = new AssetService();
-    const [asset, setAsset] = useState(null);
+    const itemService = new InventoryService();
+    const [items, setItems] = useState(null);
 
     const convertData = response => {
         return response?.data.map(x => [
-            x.code,
-            x.value,
-            x.description,
+            x.name,
+            x.about,
+            x.acquisition,
+            x.serial,
+            x.conditionUse,
             x.lastModified,
-            //x.isActive,
-            x.auditUser,
-            x?.group?.value ?? 'NA'
         ]);
     }
 
-    const getAssetInfo = async (row) => {
-        console.log(row);
-
-        var res = await assetService.getAsset(row[0]);
-
-        if(res?.status == C.status.common.ok){
-            setAsset(res.data);
-        }
-
-    };
+    // const getItemInfo = async (row) => {
+    //     console.log(row);
+    //     var res = await itemService.getItem(row[0]);
+    //     if(res?.status == C.status.common.ok){
+    //         setItems(res.data);
+    //     }
+    // };
 
     const columns = [
         {
-            name: 'Codigo',
+            name: 'Name',
             selector: row => row[0],
             sortable: true,
         },
         {
-            name: 'Ubicacion',
+            name: 'About',
             selector: row => row[1],
             sortable: true,
         },
         {
-            name: 'Descripcion',
+            name: 'Acquisition',
             selector: row => row[2],
             sortable: true,
         },
         {
-            name: 'Fecha de Modificacion',
+            name: 'Serial',
             selector: row => row[3],
             sortable: true,
         },
         {
-            name: 'Auditor',
+            name: 'ConditionUse',
+            selector: row => row[4],
+            sortable: true,
+        },
+        {
+            name: 'LastModified',
             selector: row => row[4],
             sortable: true,
         },
@@ -62,15 +63,14 @@ const Historical = () => {
     return (
         <div className="mx-4 sm:mx-auto h-auto">
             <div className="h-auto text-3xl mb-6">Historicos</div>
-
             <div className="grid grid-cols-4 gap-4 md:auto-cols-min">
                 <div className="col-span-4 rounded-md shadow-md bg-white p-6" >
                     <CustomTable
                         title={'Lista Equipos'}
                         columns={columns}
                         styles={C.styles.dataTable}
-                        onSelectRow={getAssetInfo}
-                        onHook={async () => await assetService.getAssets({})}
+                        //onSelectRow={getItemInfo}
+                        onHook={async () => await itemService.getItems({})}
                         convertData={convertData}
                     />
                     
